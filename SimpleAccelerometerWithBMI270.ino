840ca9c0576dccd0509068c1bc85bf520815663b
crowNiceToMeetYou <113548604+crowNiceToMeetYou@users.noreply.github.com>
2024-02-01 15:32:51 -0500

    Added SImpleAccelerometer with correct library
    When using the Accelerometer on the Nano 33 BLE Sense you need the BMI270_BMM150 not the LSM9DS1

---
+++ SimpleAccelerometerWithBMI270.ino
@@ -0,0 +1,36 @@
+
+
+#include "Arduino_BMI270_BMM150.h"
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+  Serial.println("Started");
+
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU!");
+    while (1);
+  }
+
+  Serial.print("Accelerometer sample rate = ");
+  Serial.print(IMU.accelerationSampleRate());
+  Serial.println(" Hz");
+  Serial.println();
+  Serial.println("Acceleration in G's");
+  Serial.println("X\tY\tZ");
+  delay(1000);
+}
+
+void loop() {
+  float x, y, z;
+
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(x, y, z);
+
+    Serial.print(x);
+    Serial.print('\t');
+    Serial.print(y);
+    Serial.print('\t');
+    Serial.println(z);
+  }
+}
